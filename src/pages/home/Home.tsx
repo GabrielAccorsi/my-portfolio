4fb6f701b6a444ee16070f43e773fb54150573a8
Gabriel Accorsi <gabrielpaccorsi@email.com>
2026-04-19 19:07:14 -0300

    feat: create AnimatedBackground component

---
+++ src/pages/home/Home.tsx
@@ -0,0 +1,13 @@
+import Hero from "../../components/sections/Hero/Hero"
+import NavBar from "../../components/Navbar/NavBar"
+const Home = () => {
+
+  return (
+    <>
+      <NavBar/>
+      <Hero />
+    </>
+  )
+}
+
+export default Home
